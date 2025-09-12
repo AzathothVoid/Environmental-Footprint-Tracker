f7230a26f20ea5d02515fb4cec8300be9eb2fa88
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-09-12 23:43:26 +0500

    creating identity folder structure

---
+++ Domain/Entities/Common/AuditableBaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Common
+{
+    public abstract class AuditableBaseEntity : BaseEntity, IAuditableBaseEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+    }
+}
---
+++ Domain/Entities/FootprintEntry.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class FootprintEntry : AuditableBaseEntity
+    {
+        public string? UserId { get; set; }            
+        public FootprintType Type { get; set; }
+        public DateTimeOffset Date { get; set; }       
+        public decimal Amount { get; set; }            
+        public string Unit { get; set; } = "";         
+        public decimal EmissionKgCO2e { get; set; }   
+        public string? RegionCode { get; set; }        
+        public string? DetailsJson { get; set; }       
+    }
+
+    public enum FootprintType { Electricity, Transport, Food, Waste }
+}
