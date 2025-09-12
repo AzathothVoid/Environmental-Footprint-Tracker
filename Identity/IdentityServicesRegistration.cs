0bae52f2f968b0f74bfb04d5a5d8a919ca9803a6
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-09-12 23:59:48 +0500

    jwt added

--- Identity/IdentityServicesRegistration.cs
+++ Identity/IdentityServicesRegistration.cs
@@ -1,9 +1,13 @@
-﻿using Application.Models.Identity;
+﻿using Application.Contracts.Identity;
+using Application.Models.Identity;
 using Identity.Models;
+using Identity.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ Identity/Services/AuthService.cs
@@ -0,0 +1,148 @@
+﻿using Application.Contracts.Identity;
+using Application.Exceptions;
+using Application.Models.Identity;
+using Identity.Models;
+using Infrastructure.Hasher;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IOptions<JwtSettings> _jwtSettings;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthService(UserManager<ApplicationUser> userManager, IOptions<JwtSettings> jwtSettings, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _jwtSettings = jwtSettings;
+            _signInManager = signInManager;
+        }
+        public async Task<AuthResponse> Login(AuthRequest request)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException()
+                { 
+                    Email = request.Email,
+                    Message = "User not found", 
+                };
+
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, request.Password, false, lockoutOnFailure: false);
+
+            if(!result.Succeeded)
+            {
+                var ex = new AuthCredentialsException()
+                {
+                    Message = "Invalid Passsword",
+                };
+                ex.IncorrectCredentials.Add("Password");
+                throw ex;
+            }
+
+            JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+
+            AuthResponse response = new AuthResponse
+            {
+                Id = user.Id,
+                Token= new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                Email = user.Email,
+                Username=user.UserName,
+            };
+
+            return response;
+        }
+
+        public async Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            var userEmail = await _userManager.FindByEmailAsync(request.Email);
+            var userUsername = await _userManager.FindByNameAsync(request.UserName);
+
+            if (userEmail != null)
+            {
+                throw new AuthCredentialsException()
+                {
+
+                };
+            }
+
+            if (userUsername != null)
+            {
+                throw new AuthCredentialsException()
+                {
+
+                };
+            }
+
+
+            var newUser = new ApplicationUser
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                UserName = request.UserName,
+                EmailConfirmed = false,
+            };
+
+            var result = await _userManager.CreateAsync(newUser, request.Password);
+
+            if (!result.Succeeded)
+                throw new Exception(string.Join(", ", result.Errors.Select(e => e.Description)));
+
+            var response = new RegistrationResponse
+            {
+                UserId = newUser.Id
+            };
+
+            return response;
+        }
+
+        private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = new List<Claim>();
+
+            for (int i = 0; i < roles.Count; i++)
+            {
+                roleClaims.Add(new Claim("roles", roles[i]));
+            }
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            }.Union(userClaims).Union(roleClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Value.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Value.Issuer,
+                audience: _jwtSettings.Value.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.Value.DurationInMinutes),
+                signingCredentials: signingCredentials
+            );
+
+            return jwtSecurityToken;
+        }
+    }
+}
