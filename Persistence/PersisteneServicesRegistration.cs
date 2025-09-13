9236b836e88e59e22945e2e975d6d19fcdf529f5
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-09-13 23:51:33 +0500

    adding persistence db context

---
+++ Persistence/PersisteneServicesRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class PersisteneServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EnvironmentalFootprintTrackerDbContext>(options =>
+                options.UseNpgsql(
+                    configuration.GetConnectionString("AIPortfolioMainDbConectionString")));
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+     
+            return services;
+        }
+    }
+}
