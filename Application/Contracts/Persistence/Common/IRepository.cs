dd653831d3c17335387081a2a4f43b364766b87c
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-09-13 23:55:08 +0500

    added common repository

---
+++ Application/Contracts/Persistence/Common/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Application.Contracts.Persistence.common
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<bool> Exists(int id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
--- Persistence/PersisteneServicesRegistration.cs
+++ Persistence/PersisteneServicesRegistration.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Application.Contracts.Persistence.common;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Persistence.Repositories.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
