1a3487af912b8b0b8b19983c992c0539e9ed90be
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-09-14 23:51:30 +0500

    added responses

---
+++ Application/Responses/CustomQueryResponse.cs
@@ -0,0 +1,14 @@
+﻿using Application.Responses.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Responses
+{
+    public class CustomQueryResponse<T> : BaseResponse where T : class
+    {
+        public T? Data { get; set; }
+    }
+}
