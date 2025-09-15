c147acd1c2571fb39ecfd492973f3e6db9126cf7
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-09-16 00:00:02 +0500

    update

---
+++ Persistence/EnvironmentalFootprintTrackerDesignTimeDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence
+{
+    public class EnvironmentalFootprintTrackerDesignTimeDbContextFactory : IDesignTimeDbContextFactory<EnvironmentalFootprintTrackerDbContext>
+    {
+        public EnvironmentalFootprintTrackerDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../WebUI");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+
+
+            var conn = configuration.GetConnectionString("CodeSnippetManagerMainDbConnectionString");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EnvironmentalFootprintTrackerDbContext>();
+
+
+            optionsBuilder.UseNpgsql(conn);
+
+            return new EnvironmentalFootprintTrackerDbContext(optionsBuilder.Options);
+        }
+    }
+}
