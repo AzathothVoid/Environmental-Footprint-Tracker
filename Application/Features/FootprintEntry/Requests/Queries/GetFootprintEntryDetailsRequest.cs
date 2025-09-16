4d9f5e9e9b63e5981a1283e4dc23b512dd6d7923
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2025-09-16 23:42:48 +0500

    added cqrs request handler folder structure

---
+++ Application/Features/FootprintEntry/Requests/Queries/GetFootprintEntryDetailsRequest.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs.FootprintEntry;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.FootprintEntry.Requests.Queries
+{
+    public class GetFootprintEntryDetailsRequest : IRequest<CustomQueryResponse<FootprintEntryDto>>
+    {
+        public int id { get; set; }
+    }
+}
---
+++ Application/Features/FootprintEntry/Requests/Queries/GetFootprintEntryListRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.Responses;
+using Application.DTOs.FootprintEntry;
+
+namespace Application.Features.FootprintEntry.Requests.Queries
+{
+    public class GetFootprintEntryListRequest : IRequest<CustomQueryResponse<List<FootprintEntryDto>>>
+    {
+
+    }
+}
